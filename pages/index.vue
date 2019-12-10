9763fcc6cb86210437fac9b36bb994069931326b
qinshengwei <15138798639@163.com>
2019-12-10 18:22:39 +0800

    add nuxt vant postcss

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <logo />
+    <nuxt-link to="/about" tag="span"
+      ><van-button type="primary">默认按钮 </van-button></nuxt-link
+    >
+
+    <p>hello world</p>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+<style>
+p {
+  width: 180px;
+}
+</style>
